545fd294b05c20eba63fd1b51ed2a93df870b96d
IMP1 <huw_taylor@hotmail.co.uk>
2022-04-05 13:03:00 +0100

    Refactor code

--- Menus/Menu.gd
+++ Menus/Menu.gd
@@ -19,8 +19,7 @@ func load_party_screen():
 	screen_loaded = ScreenLoaded.PARTY_SCREEN
 	var party_screen = PokemonPartyScreen.instance()
 	add_child(party_screen)
-	
-	
+
 func unload_party_screen():
 	menu.visible = true
 	screen_loaded = ScreenLoaded.JUST_MENU
